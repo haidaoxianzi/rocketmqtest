bc587ea9df50f19665522857c7ff0e970edd4c56
Nancy <2402774969@qq.com>
2024-10-03 15:58:50 +0800

    init env

---
+++ src/main/java/org/example/rocketmqtest/RocketmqtestApplication.java
@@ -0,0 +1,13 @@
+package org.example.rocketmqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketmqtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqtestApplication.class, args);
+    }
+
+}
